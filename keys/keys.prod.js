185fb7f5e94f9ac29b89aed1a5ef662a4a8c2d44
Kirill <kirill.deykun1@gmail.com>
2022-04-24 16:30:02 +0700

    v1.0

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -6,5 +6,6 @@ module.exports = {
     EMAIL_HOST: process.env.EMAIL_HOST,
     EMAIL_FROM: process.env.EMAIL_FROM,
     EMAIL_TO: process.env.EMAIL_TO,
-    SECRET_KEY: process.env.SECRET_KEY
+    SECRET_KEY: process.env.SECRET_KEY,
+    SITE_KEY: process.env.SITE_KEY
 }
